079fdf8b6ff7b4dabd051e5652dc471cdb6cb125
colin <colin.reitman87@gmail.com>
2020-02-18 12:38:42 -0500

    add routes for users and daily logs

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require("express");
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+const bodyParser = require('body-parser');
+const db = require('./config/keys').mongoURI;
+
+const users = require("./routes/api/users");
+const dailyLogs = require("./routes/api/dailyLogs");
 
 const app = express();
-const db = require('./config/keys').mongoURI;
 
 mongoose
   .connect(db, { useNewUrlParser: true })
@@ -10,6 +14,11 @@ mongoose
   .catch(err => console.log(err));
 
 app.get("/", (req, res) => res.send("Hello World"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use("/api/users", users);
+app.use("/api/dailyLogs", dailyLogs);
 
 const port = process.env.PORT || 5000;
 
